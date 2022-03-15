e8a0fcefc6d33b21add60f799892b90af31478a3
Renan Sampaio <74061058+renan31@users.noreply.github.com>
2022-03-15 02:34:24 -0300

    Update digital_rock_conversion.py

--- Digital rocks/digital_rock_conversion.py
+++ Digital rocks/digital_rock_conversion.py
@@ -1,26 +1,29 @@
-# Importing libraries 
+# Importing libraries
 import cv2
 import numpy as np
 import pandas as pd
 
 # Reading the image file
-data = np.fromfile("YOUR_FILE.RAW")  # Input your file
-data = data.reshape("X_DIMENSION","Y_DIMENSION","Z_DIMENSION") # Input the dimensions of your file's sample
+data = np.fromfile("YOUR_IMAGE_FILE.RAW") # Input your image file
+
+data = data.reshape(X_DIMENSION, Y_DIMENSION, Z_DIMENSION) # Input the dimensions of your file
+
 data[np.isnan(data)] = -1
+ 
 p = list()
-for k in range("Z_DIMENSION"):
-    for j in range("Y_DIMENSION"):
-        for i in range("X_DIMENSION"):
+for k in range(Z_DIMENSION):
+    for j in range(Y_DIMENSION):
+        for i in range(X_DIMENSION):
             if data[i,j,k] != -1:
-                p.append([i, j, k])
-            
-# Importing p as a Pandas dataframe
-rock = pd.DataFrame(p, columns=["x", "y", "z"])
+                p.append(["C", i, j, k])
 
-# Adding a new column (of "atoms", from the structure of .xyz files, here I used carbon but you can use any atom of your choice) 
-rock["atom"] = "C"
-columns = ["atom", "x", "y", "z"]
-rock = rock[columns]
+# Using NumPy to insert two new rows
+n_grains = len(p) 
+rock = np.array(p)
+line_1 = [n_grains, None, None, None] # <---- Number of grains
+line_2 = [None, None, None, None] # <---- Coments line
+rock = np.r_[[line_2], rock]
+rock = np.r_[[line_1], rock]
 
-# Saving the dataframe as an .xyz file
-rock.to_csv("rock.xyz", sep = " ", index = False, header = False)
+# Using Pandas to save the data into an .xyz file
+pd.DataFrame(rock).to_csv("rock.xyz", sep=" ", index=False, header=False)
